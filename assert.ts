3f2a1fd7ef950406786d2d2b811037b92863dea7
smoreira1 <smoreira1@live.com>
2022-09-06 21:57:03 -0400

    2 files modified

--- assert.ts
+++ assert.ts
@@ -2,5 +2,5 @@ export function assert(func: Function, params: Array<any>, expectedValue: any) {
   const valueRecieved = func(...params);
   return `<p>Passing: <b>${
     valueRecieved === expectedValue
-  }</b>.Got: ${valueRecieved}.Expected ${expectedValue}`;
+  }</b>. || Got: ${valueRecieved}.Expected ${expectedValue}`;
 }
--- problems/romainToInteger.ts
+++ problems/romainToInteger.ts
@@ -20,6 +20,14 @@ function example1() {
   return assert(romanToInt, ['III'], 3);
 }
 
+function example2() {
+  return assert(romanToInt, ['LVIII'], 58);
+}
+
+function example3() {
+  return assert(romanToInt, ['MCMXCIV'], 1994);
+}
+
 export function romainToIntResults() {
-  return `<p><b>Roman to Int:</b></p> ${example1()}`;
+  return `<p><b>Roman to Int:</b></p> ${example1()} ${example2()} ${example3()}`;
 }
