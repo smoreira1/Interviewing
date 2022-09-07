acf2e4f80aae4cc2c510e9f5f0b3a91e47e17dd5
smoreira1 <smoreira1@live.com>
2022-09-06 23:09:52 -0400

    1 file modified

--- problems/longestCommonPrefix.ts
+++ problems/longestCommonPrefix.ts
@@ -1,19 +1,22 @@
 import { assert } from '../assert';
 
 function longestCommonPrefix(strs: string[]): string {
-  let longestPrefix = [];
-  let currentPrefix = [];
+  let longestPrefix: Array<any> = [];
+  let currentPrefix: Array<any> = [];
   for (let i = 0; i < strs.length; i++) {
     const s1 = strs[0].split('');
     currentPrefix = [];
     for (let j = i + 1; j < strs.length; j++) {
       const s2 = strs[j].split('');
       for (let k = 0; k < s1.length; k++) {
-        if(s1[k] === s2[k]){
+        if (s1[k] === s2[k]) {
           currentPrefix.push(s1[k]);
         }
-        if(s1[k] !== s2[k]){
-          longestPrefix = currentPrefix.length > longestPrefix.length ? currentPrefix : longestCommonPrefix;
+        if (s1[k] !== s2[k]) {
+          longestPrefix =
+            currentPrefix.length > longestPrefix.length
+              ? currentPrefix
+              : longestCommonPrefix;
           continue;
         }
       }
